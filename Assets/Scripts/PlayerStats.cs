a33f144835bc5574aac02cbbfeea10237327bb05
Tigr89 <123071532+Tigr89@users.noreply.github.com>
2026-06-03 20:37:07 +0200

    CombatScript & Player/EnemyStats
    Skapat en enkel tärning som kan skada en enhet.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static PlayerStats main;
+    private int playerHealth;
+    public int playerDamage;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (main == null) main = this;
+        else Destroy(main);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdatePlayerHealth(int amount)
+    {
+        playerHealth += amount;
+
+        if(playerHealth <= 0)
+        {
+            Debug.Log("Game Over!");
+        }
+    }
+}
